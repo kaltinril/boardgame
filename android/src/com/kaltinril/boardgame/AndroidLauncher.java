14c283d020dea0685bb2887ddf134b55ad39888e
Jeremy Swartwood <jeremy.swartwood@gmail.com>
2017-05-29 16:57:55 -0800

    Initial libgdx build

---
+++ android/src/com/kaltinril/boardgame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.kaltinril.boardgame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.kaltinril.boardgame.Boardgame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Boardgame(), config);
+	}
+}
---
+++ desktop/src/com/kaltinril/boardgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kaltinril.boardgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kaltinril.boardgame.Boardgame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Boardgame(), config);
+	}
+}
